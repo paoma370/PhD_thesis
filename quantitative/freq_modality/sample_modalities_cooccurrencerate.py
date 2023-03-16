ab9900931c6429f87a73d3fbddbc8fdd67c11174
paoma370 <32926380+paoma370@users.noreply.github.com>
2023-03-16 18:33:48 +0100

    Update sample_modalities_cooccurrencerate.py

--- quantitative/freq_modality/sample_modalities_cooccurrencerate.py
+++ quantitative/freq_modality/sample_modalities_cooccurrencerate.py
@@ -7,12 +7,13 @@ Created on Mon Oct 31 12:20:30 2022
 """
 
 ################TO CREATE A SAMPLE OF 551 SENTENCES (10% OF THE CORPUS) FOR CALCULATING CO-OCCURRENCE RATE OF TYPES OF MODALITY######################
+################SAME SCRIPT FOR A SAMPLE OF 87 SENTENCES WITH CO-OCCURRENCE################################
 
 import pandas as pd
 
 df = pd.read_csv('/home/sitel/Documents/corpus_nohisp_annota_one_marker.csv', header=None, sep='\t') # no header
 print(df)
 
-sample = df.sample(n = 551)
+sample = df.sample(n = 551) #same script but with n = 87 for sample of sentences with co-occurrence
 
 sample.to_csv('/home/sitel/Documents/sample_for_modalities.csv', header=None, sep='\t')
